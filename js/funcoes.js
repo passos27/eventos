20e72506dbe2c76e10408b3b647d069425675d36
Francisco Passos <passos.27@hotmail.com>
2018-10-29 19:48:03 -0700

    Atualização de csss e javascript

---
+++ js/funcoes.js
@@ -0,0 +1,17 @@
+function novo(){
+	var x = document.getElementById('novo');
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none"
+	}
+	/*
+	0,5 pontos extras
+	Franciley
+	andré
+	railson
+	lucas
+	Michael
+	Samuel
+	*/
+}
